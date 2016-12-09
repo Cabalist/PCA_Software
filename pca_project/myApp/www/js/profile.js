adec9241a83a39d653c6f7612407e26d6887e19c
Alexander Gilman <gilmanalex@gmail.com>
2016-12-09 01:45:48 -0800

    Added Profiel picture.

--- pca_project/myApp/www/js/profile.js
+++ pca_project/myApp/www/js/profile.js
@@ -5,6 +5,12 @@ myApp.config(function($stateProvider){
     $stateProvider
         .state('profile', {
 	    url: "/",
-	    templateUrl: '/www/partials/profile.html'
+	    templateUrl: '/www/partials/profile.html',
+	    controller: 'ProfileController'
 	});
 });
+
+
+myApp.controller('ProfileController', ['$scope', function($scope) {
+    $scope.profilePic = '/www/img/blank-profile-picture-973460_960_720.png';
+}]);
