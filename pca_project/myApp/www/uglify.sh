b7d4ee6dddb22e48a14994cffdd30aaf027af400
Alexander Gilman <gilmanalex@gmail.com>
2017-02-09 14:43:41 -0800

    Added a script to concentrate + minify all the js.

---
+++ pca_project/myApp/www/uglify.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+`npm bin`/uglifyjs2 ./app/app.js  \
+	 ./app/routes.js \
+         ./app/shared/mainController.js \
+	 ./app/components/bookkeeper/bookkeeperController.js \
+	 ./app/components/bookkeeper/payTerms/bkprPayTermsController.js \
+	 ./app/components/bookkeeper/userMngmnt/bkprUsrMgmtController.js \
+	 ./app/components/manager/managerController.js  \
+	 ./app/components/manager/donors/managerDonorsController.js  \
+	 ./app/components/manager/hours/managerHoursController.js  \
+	 ./app/components/profile/profileController.js  \
+	 ./app/components/worker/workerController.js  -o ./js/profile.min.js
