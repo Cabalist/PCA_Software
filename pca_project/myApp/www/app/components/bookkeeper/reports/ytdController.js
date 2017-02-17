51cfe5959ba8ae5b954c46718a45dad69d680e23
Alexander Gilman <gilmanalex@gmail.com>
2017-02-17 11:29:24 -0800

    Calculating org take column, and added footer row with totals.

--- pca_project/myApp/www/app/components/bookkeeper/reports/ytdController.js
+++ pca_project/myApp/www/app/components/bookkeeper/reports/ytdController.js
@@ -1,10 +1,24 @@
-myApp.controller('ytdController', ['$scope','$http','$log', function($scope,$http,$log) {
+myApp.controller('ytdController', ['$scope','$http','$log','uiGridConstants', function($scope,$http,$log,uiGridConstants) {
     $scope.$emit("selectReport",1);
     $scope.rawData = [];
+    $scope.myData = [];   
 
-    $scope.myData = [];    
-
-    function addYTDDataToChart(donations){
+    $scope.gridOptions={
+	showColumnFooter:true,
+	data: $scope.myData,
+	columnDefs:[{field:'canvasser', width:'20%'},
+		    {name:"Donor",field:'donor',aggregationType: uiGridConstants.aggregationTypes.count ,width:'20%'},
+		    {name:"Date", field:'date', width:'15%' },
+		    {name:"$", field:'value',aggregationType: uiGridConstants.aggregationTypes.sum ,width:'12%' },
+		    {name:'Cnvsr. %', field:"canvasserPrcnt", width:'12%' },
+		    {name:'Cnvsr. Take',field:"canvasserTake",aggregationType: uiGridConstants.aggregationTypes.sum, width:'10%' },
+		    {name:'Org Take', field:"orgTake",aggregationType: uiGridConstants.aggregationTypes.sum ,width:'10%' } ],
+	onRegisterApi: function(gridApi){  // this is for exposing api to other controllers...
+	    $scope.gridApi = gridApi; //Don't use it...
+	}
+    };
+    
+    function addYTDDataToGrid(donations){
 	for(var i = 0;i<donations.length;i++){
 	    var d = donations[i];
 	    var dRow = {}
@@ -12,11 +26,11 @@ myApp.controller('ytdController', ['$scope','$http','$log', function($scope,$htt
 	    dRow["donor"] =d.donor.name;
 	    dRow["date"] = d.formDate;
 	    dRow["value"]=d.value;
-	    
+
 	    if (d.payTerms != null){
-		dRow["canvaser %"] = d.payTerms.percent;
+		dRow["canvasserPrcnt"] = d.payTerms.percent;
 	    }else{
-		dRow["canvaser %"] = "Null";
+		dRow["canvasserPrcnt"] = 0;
 	    }
 
 	    if (d.payTerms!=null){
@@ -25,16 +39,20 @@ myApp.controller('ytdController', ['$scope','$http','$log', function($scope,$htt
 		dRow["canvasserTake"] = 0;
 	    }
 
+	    if (d.payTerms!=null){
+		dRow["orgTake"] = (d.value - (d.value * (d.payTerms.percent/100)).toFixed(2)).toFixed(2);
+				              
+	    }else{
+		dRow["orgTake"] = d.value;
+	    }
 
-	   
 	    $scope.myData.push(dRow);
 	}
     };
     
     $http.get('/api/rest/orgYTDDonations/' + $scope.orgId+'/'+moment().year()).then(function(data){
 	$scope.rawData = data.data;
-	addYTDDataToChart(data.data);
-
+	addYTDDataToGrid(data.data);
     });
 
 }]);
