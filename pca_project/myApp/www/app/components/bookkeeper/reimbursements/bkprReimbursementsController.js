344107981a0de0f66cadff8c53e5c557535e72a1
Alexander Gilman <gilmanalex@gmail.com>
2017-05-21 15:14:09 -0700

    Added functionality to next and prev buttons. Adding new data to grid on 'save' click.

--- pca_project/myApp/www/app/components/bookkeeper/reimbursements/bkprReimbursementsController.js
+++ pca_project/myApp/www/app/components/bookkeeper/reimbursements/bkprReimbursementsController.js
@@ -20,17 +20,24 @@ myApp.controller('BkprReimbursementsController', ['$scope','$http','$log','$stat
 	//if odd, get start of previous week.
 	if (curWeek%2!=0){
 	    $scope.period = (curWeek-1)/2;
+	    $scope.curPeriod = (curWeek-1)/2;
 	}else {
 	    $scope.period = curWeek/2;
+	    $scope.curPeriod = curWeek/2;
 	}
     }
 
-    $scope.startOfPeriod = moment($scope.selectedYear+"-01-01").add($scope.period*14-1,'days').startOf('week');
-    $scope.startOfPeriodStr = $scope.startOfPeriod.format("MMMM Do, YYYY");
+    function calculatePeriod(){
+	//Set start and end of period based on $scope.period
+	$scope.startOfPeriod = moment($scope.selectedYear+"-01-01").add($scope.period*14-1,'days').startOf('week');
+	$scope.startOfPeriodStr = $scope.startOfPeriod.format("MMMM Do, YYYY");
+	
+	$scope.endOfPeriod = $scope.startOfPeriod.clone().add(14,'days').startOf('week');
+	$scope.endOfPeriodStr = $scope.endOfPeriod.format("MMMM Do, YYYY");
+    }
 
-    $scope.endOfPeriod = $scope.startOfPeriod.clone().add(14,'days').startOf('week');
-    $scope.endOfPeriodStr = $scope.endOfPeriod.format("MMMM Do, YYYY");
-
+    calculatePeriod();
+    
     $scope.reimbValue=0;
     $scope.gridData=[];
     
@@ -53,7 +60,17 @@ myApp.controller('BkprReimbursementsController', ['$scope','$http','$log','$stat
 	$state.go('bookkeeper.reimbursements',{'canvId':String(cnvsrId),'year':String($scope.selectedYear)});
     };
 
+    $scope.goToPrev = function(){
+	$scope.period--;
+	calculatePeriod();
+    };
 
+
+    $scope.goToNext = function(){
+	$scope.period++;
+	calculatePeriod();
+    };
+    
     $scope.gridOptions={
 	showColumnFooter:true,
 	enableGridMenu: true,
@@ -77,10 +94,9 @@ myApp.controller('BkprReimbursementsController', ['$scope','$http','$log','$stat
 		     footerCellTemplate: '<div class="ui-grid-cell-contents" >{{col.getAggregationValue() | currency}}</div>',
 		     width:'16%'}],
 	onRegisterApi: function(gridApi){  // this is for exposing api to other controllers...
-	    $scope.hoursGridApi = gridApi; //Don't use it...
+	    $scope.gridApi = gridApi; //Don't use it...
 	}
     };
-
     
     function selectCanvsr(){
 	for(var i=0; i<$scope.canvassers.length; i++){
@@ -107,11 +123,11 @@ myApp.controller('BkprReimbursementsController', ['$scope','$http','$log','$stat
 	selectCanvsr();
     });
 
-    function filterReimbursements(){
+    function filterReimbursements(reimbData){
 	var results = [] ;
-	for (var i=0;i<$scope.rawReimbursements.length;i++){
-	    if(($scope.selectedCnvsr.userInfo.pk == 0 )|| ($scope.selectedCnvsr.userInfo.pk==$scope.rawReimbursements[i].user.pk)){
-		var temp = $scope.rawReimbursements[i];
+	for (var i=0;i<reimbData.length;i++){
+	    if(($scope.selectedCnvsr.userInfo.pk == 0 )|| ($scope.selectedCnvsr.userInfo.pk==reimbData[i].user.pk)){
+		var temp = reimbData[i];
 
 		var worker = temp.user.first_name+" " +temp.user.last_name;
 
@@ -123,17 +139,11 @@ myApp.controller('BkprReimbursementsController', ['$scope','$http','$log','$stat
 	return results;
     };
 
-
     $http.get('/api/rest/reimbursements/' + $scope.orgId+"/"+$scope.selectedYear).then(function(data){
-	$scope.rawReimbursements = data.data;
+	$scope.gridData = filterReimbursements(data.data);
 	
-	$scope.gridData = filterReimbursements();
-	
-	$scope.gridOptions.data = $scope.gridData ;
-	
+	$scope.gridOptions.data = $scope.gridData ;	
     });
-
-
     
     $scope.saveReimbursement = function(){
 	var myData = {'worker' : $scope.selectedCnvsr.userInfo.pk,
@@ -144,9 +154,19 @@ myApp.controller('BkprReimbursementsController', ['$scope','$http','$log','$stat
 		    'value' : $scope.reimbValue }
 
 	$http.post('/api/rest/reimbursements/'+$scope.orgId+"/"+$scope.selectedYear,JSON.stringify(myData)).then(function(data){
-	    $log.log(data.data);
+	    //TODO:
+	    var newData = filterReimbursements([data.data])
+	    $scope.gridData.push(newData[0]);
+
+	    //reset val
+	    $scope.reimbValue=0;
+
+	    //refresh grid;
+	    if(typeof($scope.gridApi)!='undefined'){
+		$scope.gridApi.core.refresh();
+	    }
 	});
-	
     };
+    
     $log.log("Hello from Bookkeeper Reimbursements controller");
 }]);
